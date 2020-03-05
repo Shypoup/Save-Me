b35e1252c89b25badd9e58efadc85011a16eb65f
Hesham Gamal <38221402+Shypoup@users.noreply.github.com>
2020-03-06 01:13:55 +0200

    Handle some errors

--- android/app/src/main/java/com/saveme/MainApplication.java
+++ android/app/src/main/java/com/saveme/MainApplication.java
@@ -4,7 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.horcrux.svg.SvgPackage;
+// import com.horcrux.svg.SvgPackage;
 import com.rnfs.RNFSPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.imagepicker.ImagePickerPackage;
